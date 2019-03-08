95707cc7cfb35d3261d03c30da711cd5cac59cde
Sergiusz <serg.dabek@gmail.com>
2019-03-09 00:25:47 +0100

    add examples of datatypes and operators

---
+++ 05_strings/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //byte
+        //short
+        //int
+        //long
+        //float
+        //double
+        //char
+        //boolean
+
+        String myString = "This is a string";
+        System.out.println("my string is equal to " + myString);
+        myString = myString + " , and this is more.";
+        System.out.println("my string is equal to " + myString);
+        myString = myString + " \u00A9 2015";
+        System.out.println("my string is equal to " + myString);
+
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println("The result is " + numberString);
+
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("Last string is " + lastString);
+        double doubleNumber = 120.47;
+        lastString = lastString + doubleNumber;
+        System.out.println("LastString value " + lastString);
+
+    }
+}
