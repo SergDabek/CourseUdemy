0b0b04d3a92c55f2db4489e4f7d7fb9a1968ff67
Sergiusz <serg.dabek@gmail.com>
2019-03-10 20:20:24 +0100

    add examples of ifkeyword and codeblocks

---
+++ 09_ifkeyword_and_codeblocks/src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+//        if (score < 5000 && score > 1000){
+//                System.out.println("Your score was less than 5000 but greater than 1000");
+//        }else if (score < 1000){
+//                System.out.println("Your score was less than 1000");
+//
+//        }else {
+//                System.out.println("You got here");
+//        }
+
+        if (gameOver){
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was : " + finalScore);
+        }
+
+        boolean newGameOver = true;
+        int secondScore = 10000;
+        int secondLevelCompleted = 8;
+        int secondBonus = 200;
+
+        if (newGameOver){
+            int myFinalScore = secondScore + (secondLevelCompleted * secondBonus);
+            System.out.println("My final score was : " + myFinalScore);
+        }
+    }
+}
