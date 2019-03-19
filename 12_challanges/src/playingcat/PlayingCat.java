15739d9ba128f8e33d2b9d70cfb8e67e5a8223d2
Sergiusz <serg.dabek@gmail.com>
2019-03-19 15:22:19 +0100

    add challanges

---
+++ 12_challanges/src/playingcat/PlayingCat.java
@@ -0,0 +1,29 @@
+package playingcat;
+
+public class PlayingCat {
+
+    public static void main(String[] args) {
+
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 25));
+
+    }
+
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        if (summer) {
+            if (temperature >= 25 && temperature <= 45) {
+                return true;
+            }
+        }else if (!summer) {
+                if (temperature >= 25 && temperature <= 35) {
+                    return true;
+                }
+            }
+        return false;
+        }
+    }
+
+
+//return (temperature <=35 && temperature >= 25) || (summer) && (temperature <=45 && temperature >= 25);
+
