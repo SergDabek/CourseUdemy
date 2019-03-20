a4cf854270e25e5868cae915bcc646a2fabf4fb2
Sergiusz <serg.dabek@gmail.com>
2019-03-20 15:25:21 +0100

    add examples of for statement'

--- 14_for_statement/src/courseudemy/Main.java
+++ 14_for_statement/src/courseudemy/Main.java
@@ -8,6 +8,60 @@ public class Main {
         System.out.println("10.000 at 4% interest = " + calculateInterest(10000.0, 4.0));
         System.out.println("10.000 at 5% interest = " + calculateInterest(10000.0, 5.0));
 
+
+
+        for (int i=0; i<5; i++){
+            System.out.println("Loop " + i + " hello!");
+        }
+
+        for (int i=2; i<9; i++){                                                  //output with just two decimal points.
+            System.out.println("10.000 at " + i + " interest = " + String.format("%.2f",calculateInterest(10000.0, i)));
+        }
+
+        System.out.println("*****************************");
+
+        for (int i=8; i>1; i--){                                                  //output with just two decimal points.
+            System.out.println("10.000 at " + i + " interest = " + String.format("%.2f",calculateInterest(10000.0, i)));
+        }
+
+        System.out.println("*****************************");
+
+        for (int i=8; i>=2; i--){                                                  //output with just two decimal points.
+            System.out.println("10.000 at " + i + " interest = " + String.format("%.2f",calculateInterest(10000.0, i)));
+        }
+
+        System.out.println("*****************************");
+
+        int count = 0;
+        for (int i=10; i <= 50; i++){
+            if(isPrime(i)){
+                count++;
+                System.out.println("Number " + i + " is a prime number");
+                if (count == 10){
+                    System.out.println("Exiting for loop");
+                    break;
+                }
+            }
+        }
+
+    }
+
+
+    public static boolean isPrime(int n){
+
+        if (n == 1){
+            return false;
+        }
+                                    //n/2
+        for (int i=2; i <= (long)Math.sqrt(n); i++){
+            System.out.println("Looping " + i);
+            if (n % i == 0){
+                return false;
+            }
+
+        }
+
+        return true;
     }
 
     public static double calculateInterest(double amount, double interestRate){
